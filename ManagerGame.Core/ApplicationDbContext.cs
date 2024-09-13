ae3356f2d761214711ff146fed4aa638cd7ee03a
Jakob Csörgei Gustavsson <jakob@sigmastocks.com>
2024-09-13 03:17:39 +0200

    add players table

--- ManagerGame.Core/ApplicationDbContext.cs
+++ ManagerGame.Core/ApplicationDbContext.cs
@@ -7,6 +7,7 @@ public class ApplicationDbContext(DbContextOptions<ApplicationDbContext> options
 {
     public DbSet<Manager> Managers { get; set; }
     public DbSet<Team> Teams { get; set; }
+    public DbSet<Player> Players { get; set; }
 
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
     {
@@ -20,10 +21,21 @@ public class ApplicationDbContext(DbContextOptions<ApplicationDbContext> options
             .WithOne()
             .HasForeignKey(x => x.ManagerId)
             .IsRequired();
+        modelBuilder.Entity<Manager>()
+	        .Property(x => x.Email)
+	        .HasConversion(x => x.EmailAddress, x => new Email(x));
+        modelBuilder.Entity<Manager>()
+	        .Property(x => x.Name).HasConversion(x => x.Name, x => new ManagerName(x));
 
-        modelBuilder.Entity<Manager>().Property(x => x.Email).HasConversion(x => x.EmailAddress, x => new Email(x));
-        modelBuilder.Entity<Manager>().Property(x => x.Name).HasConversion(x => x.Name, x => new ManagerName(x));
+        modelBuilder.Entity<Team>()
+	        .Property(x => x.Name)
+	        .HasConversion(x => x.Name, x => new TeamName(x));
+        modelBuilder.Entity<Team>()
+	        .HasMany(x => x.Players)
+	        .WithOne()
+	        .HasForeignKey(x => x.TeamId);
 
-        modelBuilder.Entity<Team>().Property(x => x.Name).HasConversion(x => x.Name, x => new TeamName(x));
+        modelBuilder.Entity<Player>()
+	        .HasKey(x => x.Id);
     }
 }
