14b7ce9c9031fe8eef472006d29e55a0922ed697
Jakob Csörgei Gustavsson <soulrivers@gmail.com>
2024-09-17 17:29:37 +0200

    use mocking repo instead of fakerepo
    couldn't replicate foreign key constraints properly with fakerepo

--- ManagerGame.Test/Commands/SignPlayerCommandHandlerTest.cs
+++ ManagerGame.Test/Commands/SignPlayerCommandHandlerTest.cs
@@ -1,5 +1,6 @@
 using ManagerGame.Core.Commands;
 using ManagerGame.Core.Domain;
+using NSubstitute;
 
 namespace ManagerGame.Test.Commands;
 
@@ -8,19 +9,21 @@ public class SignPlayerCommandHandlerTest
 	[Fact]
 	public async Task PersistsPlayerInTeam()
 	{
-		var teamRepo = new FakeRepo<Team>();
+		var teamRepo = Substitute.For<IRepository<Team>>();
 		var team = Team.Create(new TeamName("Laget"), Guid.NewGuid(), []);
-		await teamRepo.Add(team);
-		var playerRepo = new FakeRepo<Player>();
+		teamRepo.Find(team.Id).Returns(team);
+		
+		var playerRepo = Substitute.For<IRepository<Player>>();
 		var player = new Player(Guid.NewGuid(), team.Id, new PlayerName("Jakob"), Position.Defender);
-		await playerRepo.Add(player);
+		playerRepo.Find(player.Id).Returns(player);
+		
 		var sut = new SignPlayerCommandHandler(playerRepo, teamRepo);
 
 		await sut.Handle(new SignPlayerRequest(team.Id, player.Id));
 
-		var signedPlayer = (await teamRepo.Find(team.Id))!.Players.First();
-		Assert.Equal(player.Id, signedPlayer.Id);
-		Assert.Equal("Jakob", signedPlayer.Name.Name);
-		Assert.Equal(Position.Defender, signedPlayer.Position);
+		await teamRepo.Received().Update(Arg.Is<Team>(t =>
+			t.Players.First().Id == player.Id
+			&& t.Players.First().Name.Name == "Jakob"
+			&& t.Players.First().Position == Position.Defender));
 	}
 }
