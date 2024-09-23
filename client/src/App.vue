41d4a87f56e9e46d22eded1481902cf52c43b5fc
Jakob Csörgei Gustavsson <soulrivers@gmail.com>
2024-09-23 15:59:57 +0200

    add frontend with basic signalr functionality

---
+++ client/src/App.vue
@@ -0,0 +1,63 @@
+<script setup lang="ts">
+import HelloWorld from './components/HelloWorld.vue'
+import * as signalR from "@microsoft/signalr";
+import {ref} from "vue";
+
+const connection = new signalR.HubConnectionBuilder()
+    .withUrl("http://localhost:5001/chatHub")
+    //.configureLogging()//signalR.LogLevel.Information)
+    .build();
+
+const message = ref<string>();
+const chat = ref<string>("");
+
+connection.on("messageReceived", (user, message) => {
+  chat.value = `${chat.value}\n${user}: ${message}`;
+})
+function send() {
+  connection.send("SendMessage", "Jakob", message.value);
+  message.value = "";
+}
+
+async function start() {
+    try {
+        await connection.start();
+        console.log("SignalR Connected.");
+    } catch (err) {
+        console.log(err);
+        setTimeout(start, 5000);
+    }
+};
+
+connection.onclose(async () => {
+    await start();
+});
+
+start();
+</script>
+
+<template>
+  <div style="display: flex; flex-direction: column; width: 20rem">
+  <textarea style="height: 20rem"v-model="chat"></textarea>
+  <div>
+    <label for="text">Meddelande: </label>
+    <input @keyup.enter="send" v-model="message" id="text" type="text"/>
+    <button @click="send">Hej</button>
+  </div>
+</div>
+</template>
+
+<style scoped>
+.logo {
+  height: 6em;
+  padding: 1.5em;
+  will-change: filter;
+  transition: filter 300ms;
+}
+.logo:hover {
+  filter: drop-shadow(0 0 2em #646cffaa);
+}
+.logo.vue:hover {
+  filter: drop-shadow(0 0 2em #42b883aa);
+}
+</style>
