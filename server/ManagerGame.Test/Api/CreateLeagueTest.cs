8831eaf4cf023c8b5a52f32f21683d0c972f3357
Jakob Csörgei Gustavsson <soulrivers@gmail.com>
2024-10-22 12:12:43 +0200

    fix tests

--- server/ManagerGame.Test/Api/CreateLeagueTest.cs
+++ server/ManagerGame.Test/Api/CreateLeagueTest.cs
@@ -20,17 +20,17 @@ public class CreateLeagueTest : IClassFixture<Fixture>
         var (_, team) = await Seed.SeedAndLogin(_httpClient);
         var (createHttp, createLeagueDto) =
             await _httpClient.Post<CreateLeagueDto>("/api/leagues", new CreateLeagueRequest());
-        
+
         Assert.Equal(HttpStatusCode.OK, createHttp.StatusCode);
 
         var (admit1Http, admit1) = await _httpClient.Post<AdmitTeamDto>("/api/leagues/admitTeam",
             new AdmitTeamRequest { LeagueId = createLeagueDto!.Id, TeamId = team.Id });
-        
+
         Assert.Equal(HttpStatusCode.OK, admit1Http.StatusCode);
         Assert.Contains(admit1!.League.Teams, x => x.Id == team.Id);
-        
+
         var (_, team1) = await _httpClient.Get<TeamDto>($"/api/teams/{team.Id}");
-        Assert.Equal(createLeagueDto.Id, team1!.LeagueId);
+        Assert.Equal(createLeagueDto.Id, team1!.League!.Id);
     }
 }
 
--- server/ManagerGame.Test/Api/CreateManagerTest.cs
+++ server/ManagerGame.Test/Api/CreateManagerTest.cs
@@ -23,7 +23,7 @@ public class CreateManagerTest : IClassFixture<Fixture>
 
         Assert.Equal(HttpStatusCode.OK, createManagerResponse.StatusCode);
         Assert.NotNull(manager);
-        Assert.Equal("Jakob", manager.Name.Name);
+        Assert.Equal("Jakob", manager.Name);
         Assert.Contains("@jakobsson.com", manager.Email.EmailAddress);
         Assert.NotEqual(Guid.Empty, manager.Id);
         Assert.Single(db.Managers);
