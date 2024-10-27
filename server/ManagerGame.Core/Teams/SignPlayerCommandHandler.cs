40dc2ea59386d5f92b88bbcca21e48004dda5aa6
Jakob Csörgei Gustavsson <soulrivers@gmail.com>
2024-10-27 16:51:04 +0100

    remove test code

--- server/ManagerGame.Core/Teams/SignPlayerCommandHandler.cs
+++ server/ManagerGame.Core/Teams/SignPlayerCommandHandler.cs
@@ -1,27 +1,10 @@
 namespace ManagerGame.Core.Teams;
 
 public class SignPlayerCommandHandler(IRepository<Player> playerRepo, IRepository<Team> teamRepo)
-
 {
-    public void Apa(int a)
-    {
-    }
-
-    public void Apa(object a)
-    {
-    }
-
-    public void Apa(string a)
-    {
-    }
-
     public async Task<Result<Team>> Handle(SignPlayerRequest command,
         CancellationToken cancellationToken = default)
     {
-        Hej apa = Apa;
-        apa(1);
-
-
         var team = await teamRepo.Find(command.TeamId, cancellationToken);
         if (team is null) return Result<Team>.Failure(Error.NotFound);
 
@@ -35,6 +18,4 @@ public class SignPlayerCommandHandler(IRepository<Player> playerRepo, IRepositor
 
         return Result<Team>.Success(team);
     }
-
-    private delegate void Hej(int i);
 }
