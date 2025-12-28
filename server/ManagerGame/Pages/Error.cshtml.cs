2fa5856a2c25edc7cc538a186c54d269910be8f7
Jakob Csörgei Gustavsson <jakob.csorgei@gmail.com>
2025-12-28 22:19:36 +0100

    cleanup razor files

--- server/ManagerGame/Pages/Error.cshtml.cs
+++ server/ManagerGame/Pages/Error.cshtml.cs
@@ -1,23 +1,18 @@
 ﻿using System.Diagnostics;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
-
 namespace ManagerGame.Pages;
 
-[ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-[IgnoreAntiforgeryToken]
+[ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true), IgnoreAntiforgeryToken]
 public class ErrorModel : PageModel
 {
-    public string? RequestId { get; set; }
-
-    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
 
     private readonly ILogger<ErrorModel> _logger;
 
-    public ErrorModel(ILogger<ErrorModel> logger)
-    {
-        _logger = logger;
-    }
+    public ErrorModel(ILogger<ErrorModel> logger) => _logger = logger;
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
 
     public void OnGet()
     {
